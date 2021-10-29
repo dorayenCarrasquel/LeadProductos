c6cc0453f3429bc8b4276defa4095d887d32c975
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-10-29 14:05:30 -0300

    Salvar leadDto na lista leads

--- src/main/java/br/com/zup/Lead/Lead/LeadService.java
+++ src/main/java/br/com/zup/Lead/Lead/LeadService.java
@@ -1,2 +1,23 @@
-package br.com.zup.Lead.Lead;public class LeadService {
+package br.com.zup.Lead.Lead;
+
+import br.com.zup.Lead.Lead.Dtos.LeadDto;
+import br.com.zup.Lead.Lead.Dtos.ProductDto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LeadService {
+   private List<LeadDto> leadDtoList = new ArrayList<>();
+
+
+    public LeadDto salvarLead(LeadDto leadDto){
+        leadDtoList.add(leadDto);
+        return leadDto;
+    }
+
+
+
+
 }
