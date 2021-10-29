fcac6d75d93dbb83ef29e830f1f1625ea2de2cb6
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-10-29 15:31:53 -0300

    Comitão de Buscar Email e buscar producto

--- src/main/java/br/com/zup/Lead/Lead/LeadService.java
+++ src/main/java/br/com/zup/Lead/Lead/LeadService.java
@@ -2,7 +2,11 @@ package br.com.zup.Lead.Lead;
 
 import br.com.zup.Lead.Lead.Dtos.LeadDto;
 import br.com.zup.Lead.Lead.Dtos.ProductDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,12 +16,31 @@ public class LeadService {
    private List<LeadDto> leadDtoList = new ArrayList<>();
 
 
-    public LeadDto salvarLead(LeadDto leadDto){
+    public void salvarLead(LeadDto leadDto){
         leadDtoList.add(leadDto);
-        return leadDto;
+
+    }
+
+    public List<LeadDto> exibirDados() {
+        return leadDtoList;
     }
 
 
+    public LeadDto buscarEmail(String email, LeadDto leadDto){
+        for (LeadDto leadReferencia: leadDtoList) {
+            if (leadDto.getEmail().equalsIgnoreCase(email)){
+               return leadDto;
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
 
+    public LeadDto buscarProducto()
+
+
+    public LeadDto verificarEmail(@PathVariable String email, @RequestBody LeadDto leadDto){
+        return
+
+    }
 
 }
